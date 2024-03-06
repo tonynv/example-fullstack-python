ea961d9bea7b47fe34ae9fce38efab36b54bffb4
Tony Vattathil <t@teamroster.io>
2024-03-05 22:24:42 -0800

    Add terraform .gitignore

---
+++ terraform/variable.tf
@@ -0,0 +1,71 @@
+# provider
+
+variable "region" {
+  description = "The AWS region to create resources in."
+  default     = "us-west-1"
+}
+
+# network
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR Block for Public Subnet 1"
+  default     = "10.0.1.0/24"
+}
+variable "public_subnet_2_cidr" {
+  description = "CIDR Block for Public Subnet 2"
+  default     = "10.0.2.0/24"
+}
+variable "private_subnet_1_cidr" {
+  description = "CIDR Block for Private Subnet 1"
+  default     = "10.0.3.0/24"
+}
+variable "private_subnet_2_cidr" {
+  description = "CIDR Block for Private Subnet 2"
+  default     = "10.0.4.0/24"
+}
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["us-west-1b", "us-west-1c"]
+}
+
+# load balancer
+variable "health_check_path" {
+  description = "Health check path for the default target group"
+  default     = "/ping/"
+}
+
+# ecs
+variable "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  default     = "production"
+}
+
+# logs 
+
+variable "log_retention_in_days" {
+  default = 30
+}
+
+# ecs
+
+variable "docker_image_url_django" {
+  description = "Docker image to run in the ECS cluster"
+  default     = "<AWS_ACCOUNT_ID>.dkr.ecr.us-west-1.amazonaws.com/django-app:latest"
+}
+
+variable "app_count" {
+  description = "Number of Docker containers to run"
+  default     = 2
+}
+
+variable "fargate_cpu" {
+  description = "Amount of CPU for Fargate task. E.g., '256' (.25 vCPU)"
+  default     = "256"
+}
+
+variable "fargate_memory" {
+  description = "Amount of memory for Fargate task. E.g., '512' (0.5GB)"
+  default     = "512"
+}
+
